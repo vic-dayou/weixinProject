93c33ac7ca18ac420eec1771ca6b2bc933ecd431
yhinan <1300204051@qq.com>
2019-06-07 19:56:54 +0800

    db.sql

--- db.sql
+++ db.sql
@@ -14,7 +14,7 @@ create table `product_info`(
 create table `product_category`(
 	`category_id` int not null auto_increment,
 	`category_name` varchar(64) not null comment '类目名称',
-	`product_type` int not null comment '类目编号',
+	`category_type` int not null comment '类目编号',
 	`create_time` timestamp not null default current_timestamp comment '创建时间',
 	`update_time` timestamp not null default current_timestamp on update current_timestamp comment'更新时间',
 	primary key (`category_id`),
@@ -23,12 +23,12 @@ create table `product_category`(
 
 create table `order_master` (
 	`order_id` varchar(32) not null,
-	`buyer_name` varchar not null comment '卖家名字',
+	`buyer_name` varchar(32) not null comment '卖家名字',
 	`buyer_phone` varchar(32) not null comment '卖家电话',
 	`buyer_address` varchar(128) not null comment '买家地址',
 	`buyer_openid` varchar(64) not null comment '微信openid',
 	`order_amount` decimal(8,2) not null comment '订单总额',
-	`order_status` tinyint(3) not null comment default '0' '订单状态,默认0 新订单',
+	`order_status` tinyint(3) not null default '0' comment '订单状态,默认0 新订单',
 	`pay_status` tinyint(3) not null default '0' comment '支付状态，默认0 未支付',
 	`create_time` timestamp not null default current_timestamp comment '创建时间',
 	`update_time` timestamp not null default current_timestamp on update current_timestamp comment'更新时间',
@@ -37,7 +37,7 @@ create table `order_master` (
 )comment '订单表';
 
 
-create table 'order_detail'(
+create table `order_detail`(
 	`detail_id` varchar(32) not null,
 	`order_id` varchar(32) not null,
 	`product_id` varchar(32) not null,
@@ -47,6 +47,6 @@ create table 'order_detail'(
 	`product_icon` varchar(128) not null,
 	`create_time` timestamp not null default current_timestamp comment '创建时间',
 	`update_time` timestamp not null default current_timestamp on update current_timestamp comment'更新时间',
-	primary key ('detail_id'),
+	primary key (`detail_id`),
 	key `idx_order_id` (`order_id`)
 )comment '订单详情表';
